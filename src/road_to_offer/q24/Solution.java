0e5ec1c23b3ea9aaa3563350b33a5e99857d1a1b
my2306 <my2306@nyu.edu>
2024-03-08 23:53:03 +0800

    all questions

--- src/road_to_offer/q24/Solution.java
+++ src/road_to_offer/q24/Solution.java
@@ -48,8 +48,8 @@ public class Solution {
     static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
+
         ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
     }
 }
